395a71dce5c09de9ef0413e684826346a0a5a49e
Nayan Gupta <nayan.neema93@gmail.com>
2018-08-29 02:04:20 +0530

    Show listing of latest post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
 
   validates_presence_of :title, :url, :publish_date
 
+  delegate :title, to: :feed, allow_nil: true, prefix: true
+
+  def publish_date
+    super&.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :feeds, except: [:show]
+  root to: "posts#index"
 
+  resources :feeds, except: [:show]
+  resources :posts, only: [:index]
 end
